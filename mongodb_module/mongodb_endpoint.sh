cdcc5923614c2aebfdfa8b0bc9876e8e2f0dff7a
Ketan Joshi <ketanjoshi@Ketans-MacBook-Pro.local>
2022-11-19 00:08:51 +0530

    Fixes

--- mongodb_module/mongodb_endpoint.sh
+++ mongodb_module/mongodb_endpoint.sh
@@ -4,12 +4,12 @@ then
     echo -n "mongo1$1, " >> $3/mongodb_endpoint.txt
     for (( i=1; i<=$2; i++ ))
     do
-        echo -n "mongo$(expr $i+1)$1, " >> $3/mongodb_endpoint.txt
+        echo -n "mongo$(expr $i + 1)$1, " >> $3/mongodb_endpoint.txt
     done
 else
     echo -n "mongo1, " >> $3/mongodb_endpoint.txt
     for (( i=1; i<=$2; i++ ))
     do
-        echo -n "mongo$(expr $i+1), " >> $3/mongodb_endpoint.txt
+        echo -n "mongo$(expr $i + 1), " >> $3/mongodb_endpoint.txt
     done
 fi
